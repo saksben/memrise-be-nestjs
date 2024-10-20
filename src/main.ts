eb8f0547fab2d77e2b0f4c45bfa26f504db206bb
Benjamin Saks <saksben2@gmail.com>
2024-10-20 15:34:55 -0700

    set up prisma and main

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,22 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:3002',
+      'http://localhost:3003',
+    ],
+    methods: 'GET, POST, PATCH, DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
+  // await app.listen(3000);
+  // await app.listen(3000);
+  // await app.listen(3000);
+  await app.listen(3003);
 }
 bootstrap();
