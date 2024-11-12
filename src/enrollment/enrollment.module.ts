225a0e3774130beb6a36edb506c9701254ea9408
Benjamin Saks <saksben2@gmail.com>
2024-11-11 23:07:37 -0700

    add prismaservice

--- src/enrollment/enrollment.module.ts
+++ src/enrollment/enrollment.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EnrollmentController } from './enrollment.controller';
 import { EnrollmentService } from './enrollment.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [EnrollmentController],
-  providers: [EnrollmentService]
+  providers: [EnrollmentService, PrismaService],
 })
 export class EnrollmentModule {}
